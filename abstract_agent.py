0eddd1b0f766b16a9ddfbee6db05720ac9e74149
German Otero <unleashed.gotero@gmail.com>
2025-06-01 17:01:59 -0300

    Se implementa el método train del agente abstracto.

--- abstract_agent.py
+++ abstract_agent.py
@@ -7,16 +7,27 @@ from tqdm import tqdm
 import random
 
 
-
 class Agent(ABC):
-    def __init__(self, gym_env, obs_processing_func, memory_buffer_size, batch_size, learning_rate, gamma,
-                 epsilon_i, epsilon_f, epsilon_anneal_steps, episode_block, device):
+    def __init__(
+        self,
+        gym_env,
+        obs_processing_func,
+        memory_buffer_size,
+        batch_size,
+        learning_rate,
+        gamma,
+        epsilon_i,
+        epsilon_f,
+        epsilon_anneal_steps,
+        episode_block,
+        device,
+    ):
         self.device = device
 
         # Funcion phi para procesar los estados.
         self.state_processing_function = obs_processing_func
 
-        # Asignarle memoria al agente 
+        # Asignarle memoria al agente
         self.memory = ReplayMemory(memory_buffer_size)
 
         self.env = gym_env
@@ -26,70 +37,108 @@ class Agent(ABC):
         self.learning_rate = learning_rate
         self.gamma = gamma
 
-
         self.epsilon_i = epsilon_i
         self.epsilon_f = epsilon_f
         self.epsilon_anneal_steps = epsilon_anneal_steps
-        
+
         self.episode_block = episode_block
 
         self.total_steps = 0
-    
-    def train(self, number_episodes = 50_000, max_steps_episode = 10_000, max_steps=1_000_000):
-      rewards = []
-      total_steps = 0
-      
-      metrics = {"reward": 0.0, "epsilon": self.epsilon_i, "steps": 0}
 
-      pbar = tqdm(range(number_episodes), desc="Entrenando", unit="episode")
+    def train(
+        self, number_episodes=50_000, max_steps_episode=10_000, max_steps=1_000_000
+    ):
+        rewards = []
+        total_steps = 0
 
-      for ep in pbar:
-        if total_steps > max_steps:
-            break
-        
-        # Observar estado inicial como indica el algoritmo
-        state, _ = self.env.reset()
-        state_phi = self.state_processing_function(state)
-        current_episode_reward = 0.0
-        current_episode_steps = 0
-        done = False
+        metrics = {"reward": 0.0, "epsilon": self.epsilon_i, "steps": 0}
 
-        # Bucle principal de pasos dentro de un episodio
-        for _ in range(max_steps):
-            # TODO: Seleccionar acción epsilon-greedy usando select_action()
-            # TODO: Ejecutar action = env.step(action)
-            # TODO: Procesar next_state con state_processing_function
-            # TODO: Acumular reward y actualizar total_steps, current_episode_steps
-            # TODO: Almacenar transición en replay memory
-            # TODO: Llamar a update_weights() para entrenar modelo
-            # TODO: Actualizar state y state_phi al siguiente estado
-            # TODO: Comprobar condición de done o límite de pasos de episodio y break
-            pass
-        
-        # Registro de métricas y progreso
-        rewards.append(current_episode_reward)
-        metrics["reward"] = np.mean(rewards[-self.episode_block:])
-        metrics["epsilon"] = self.compute_epsilon(total_steps)
-        metrics["steps"] = total_steps
-        pbar.set_postfix(metrics)
+        pbar = tqdm(range(number_episodes), desc="Entrenando", unit="episode")
 
-      # Guardar el modelo entrenado  
-      torch.save(self.policy_net.state_dict(), "GenericDQNAgent.dat")
+        for ep in pbar:
+            if total_steps > max_steps:
+                break
 
-      return rewards
-    
-        
+            # Observar estado inicial como indica el algoritmo
+            state, _ = self.env.reset()
+            state_phi = self.state_processing_function(state)
+            current_episode_reward = 0.0
+            current_episode_steps = 0
+            done = False
+
+            # Bucle principal de pasos dentro de un episodio
+            for _ in range(max_steps):
+                # TODO: Seleccionar acción epsilon-greedy usando select_action()
+
+                # Seleccionamos la acción a partir de la implementación propia de
+                # la clase abstracta. Luego, cada agente deberá implementar
+                # su propia lógica para seleccionar la acción.
+
+                action = self.select_action(state_phi, total_steps, train=True)
+
+                # TODO: Ejecutar action = env.step(action)
+
+                # Ejecutamos la acción.
+
+                next_state, reward, done, _, _ = self.env.step(action)
+
+                # TODO: Procesar next_state con state_processing_function
+
+                # Debido a que el ambiente devuelve la imagen sin procesar,
+                # debemos prepocesarla para que tenga el formato correcto
+
+                next_state_phi = self.state_processing_function(next_state)
+
+                # TODO: Acumular reward y actualizar total_steps, current_episode_steps
+
+                current_episode_reward += reward
+                total_steps += 1
+                current_episode_steps += 1
+
+                # TODO: Almacenar transición en replay memory
+
+                self.memory.push(
+                    Transition(state_phi, action, reward, next_state_phi, done)
+                )
+
+                # TODO: Llamar a update_weights() para entrenar modelo
+
+                self.update_weights()
+
+                # TODO: Actualizar state y state_phi al siguiente estado
+
+                state_phi = next_state_phi
+                state = next_state
+
+                # TODO: Comprobar condición de done o límite de pasos de episodio y break
+
+                if done or current_episode_steps >= max_steps_episode:
+                    break
+
+            # Registro de métricas y progreso
+            rewards.append(current_episode_reward)
+            metrics["reward"] = np.mean(rewards[-self.episode_block :])
+            metrics["epsilon"] = self.compute_epsilon(total_steps)
+            metrics["steps"] = total_steps
+            pbar.set_postfix(metrics)
+
+        # Guardar el modelo entrenado
+        torch.save(self.policy_net.state_dict(), "GenericDQNAgent.dat")
+
+        return rewards
+
     def compute_epsilon(self, steps_so_far):
         """
         Compute el valor de epsilon a partir del número de pasos dados hasta ahora.
         """
         if steps_so_far < self.epsilon_anneal_steps:
-            epsilon = self.epsilon_i - (self.epsilon_i - self.epsilon_f) * (steps_so_far / self.epsilon_anneal_steps)
+            epsilon = self.epsilon_i - (self.epsilon_i - self.epsilon_f) * (
+                steps_so_far / self.epsilon_anneal_steps
+            )
         else:
             epsilon = self.epsilon_f
         return epsilon
-        
-    
+
     def play(self, env, episodes=1):
         """
         Modo evaluación: ejecutar episodios sin actualizar la red.
@@ -107,7 +156,7 @@ class Agent(ABC):
         """
         Selecciona una acción a partir del estado actual. Si train=False, se selecciona la acción greedy.
         Si train=True, se selecciona la acción epsilon-greedy.
-        
+
         Args:
             state: El estado actual del entorno.
             current_steps: El número de pasos actuales. Determina el valor de epsilon.
